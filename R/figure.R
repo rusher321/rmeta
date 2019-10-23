ac721bb3d746edc13491aa6240ae57111479e5b1
任华慧(Huahui Ren) <renhuahui@genomics.cn>
2019-10-23 15:33:42 +0800

    update some function

--- R/figure.R
+++ R/figure.R
@@ -1,11 +1,11 @@
 ## myfigure for metagenome
 
+#' plot volcano
 #' this function to plot volcano based ggplot2
-#'
 #' @param qdat include column name Fdr & fc
 #' @param fdcut  the foldchange cutoff
 #'
-#' @return a figure
+#' @return figure
 #' @export
 #'
 #' @examples
@@ -39,3 +39,158 @@ myvolcano <- function(qdat, fdcut){
   return(p)
 
 }
+
+
+#' Multiplot
+#' this function to plot volcano based ggplot2
+#' @param ...
+#' @param plotlist
+#' @param file
+#' @param cols
+#' @param layout
+#'
+#' @return
+#' @export
+#'
+#' @examples
+multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+  library(grid)
+
+  # Make a list from the ... arguments and plotlist
+  plots <- c(list(...), plotlist)
+
+  numPlots = length(plots)
+
+  # If layout is NULL, then use 'cols' to determine layout
+  if (is.null(layout)) {
+    # Make the panel
+    # ncol: Number of columns of plots
+    # nrow: Number of rows needed, calculated from # of cols
+    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                     ncol = cols, nrow = ceiling(numPlots/cols))
+  }
+
+  if (numPlots==1) {
+    print(plots[[1]])
+
+  } else {
+    # Set up the page
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+
+    # Make each plot, in the correct location
+    for (i in 1:numPlots) {
+      # Get the i,j matrix positions of the regions that contain this subplot
+      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+
+      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+                                      layout.pos.col = matchidx$col))
+    }
+  }
+}
+
+#' pcoaFig
+#'
+#' @param data.dist
+#' @param cluster
+#'
+#' @return figure
+#' @export
+#'
+#' @examples
+pcoaFig <- function(data.dist, cluster){
+
+  #library(RColorBrewer)
+
+  # plot the pcoa
+  obs.pcoa=dudi.pco(data.dist, scannf=F, nf=3)
+  var1 <- round((obs.pcoa$eig[1]/sum(obs.pcoa$eig))*100,2)
+  var2 <- round((obs.pcoa$eig[2]/sum(obs.pcoa$eig))*100,2)
+  minX <- min(obs.pcoa$li[,1])
+  maxX <- max(obs.pcoa$li[,1])
+  minY <- min(obs.pcoa$li[,2])
+  maxY <- max(obs.pcoa$li[,2])
+
+  plot(0,0, main = "Pcoa", type = "n",
+       xlab=paste("Pco1 (",var1,"%)"),ylab=paste("Pco2 (",var2,"%)"),
+       xlim=c(minX-10^floor(log10(abs(minX))),maxX+10^floor(log10(abs(maxX)))),
+       ylim=c(minY-10^floor(log10(abs(minY))),maxY+10^floor(log10(abs(maxY)))),
+       frame=TRUE, cex=0.5, add=T)
+  s.class(obs.pcoa$li, fac=as.factor(cluster), cell =2 ,
+          csta = 0 , col = brewer.pal(8,'Set2'), grid=F, add.plot = T)
+
+
+}
+
+
+#' nmdsFig
+#'
+#' @param data
+#' @param method
+#' @param config
+#'
+#' @return figure
+#' @export
+#'
+#' @examples
+
+nmdsFig <- function(data, method = "bray", config){
+
+  mds <- metaMDS(data, distance = method, k = 2, trymax = 50)
+
+  dat2 <- data.frame(mds$point[,1:2])
+
+  minX <- min(dat2[,1])
+  maxX <- max(dat2[,1])
+  minY <- min(dat2[,2])
+  maxY <- max(dat2[,2])
+
+  plot(0,0, main = "Pcoa", type = "n",
+       xlab=paste("Pco1 (",var1,"%)"),ylab=paste("Pco2 (",var2,"%)"),
+       xlim=c(minX-5^floor(log10(abs(minX))),maxX+5^floor(log10(abs(maxX)))),
+       ylim=c(minY-5^floor(log10(abs(minY))),maxY+5^floor(log10(abs(maxY)))),
+       frame=TRUE, cex=0.5, add=T)
+  s.class(dat2, fac=as.factor(config), cell =2 ,
+          csta = 0 , col = brewer.pal(8,'Set2'), grid=F, add.plot = T)
+
+
+}
+
+
+#' nmdsFigEx
+#' plot the nmds figure
+#' @param data
+#' @param method
+#' @param config
+#' @param color
+#'
+#' @return
+#' @export
+#'
+#' @examples
+nmdsFigEx <- function(data, method = "bray", config, color = c("#66C2A5","#E78AC3")){
+
+  mds <- metaMDS(data, distance = method, k = 2, trymax = 50)
+
+  dat2 <- data.frame(mds$point[,1:2])
+  colnames(dat2) <- c("NMDS1", "NMDS2")
+  # to confirm the config plot
+  dat2$group <- as.factor(config[,1])
+  dat2$group2 <- as.factor(config[,2])
+  plot <- ggplot(dat2,aes(NMDS1, NMDS2,color=group))+geom_point()+
+    stat_ellipse(level = 0.7, show.legend = F)+
+    ggtitle(paste0("NMDS","_",method))+
+    theme(plot.title = element_text(hjust = 0.5,size = 14),
+          axis.title = element_text(size = 10),
+          axis.text = element_text(size = 13),
+          panel.grid = element_blank(),
+          legend.title = element_text(size = 15),
+          legend.text = element_text(size = 13))+
+    scale_color_manual(values = color)+facet_wrap(.~group2)
+
+  return(plot)
+
+}
+
+
+
---
+++ R/stat.R
@@ -0,0 +1,185 @@
+ ## stat for metagenome
+#' JSD distance
+#' to compute the jsd distance
+#' @param inMatrix
+#' @param pseudocount
+#' @param ...
+#'
+#' @return distance
+#' @export
+#'
+#' @examples
+distJSD <- function(inMatrix, pseudocount=0.00000001, ...) {
+
+  # if have negtive number, transform them
+  if(any(inMatrix < 0)){
+    mixV <- min(inMatrix)
+    inMatrix <- inMatrix+abs(mixV)+pseudocount
+  }
+
+  inMatrix <- t(inMatrix)
+  KLD <- function(x,y) sum(x *log(x/y))
+  JSD<- function(x,y) sqrt(0.5 * KLD(x, (x+y)/2) + 0.5 * KLD(y, (x+y)/2))
+  matrixColSize <- length(colnames(inMatrix))
+  matrixRowSize <- length(rownames(inMatrix))
+  colnames <- colnames(inMatrix)
+  resultsMatrix <- matrix(0, matrixColSize, matrixColSize)
+  min <- min(inMatrix)
+  inMatrix <- inMatrix+min
+  inMatrix = apply(inMatrix,1:2,function(x) ifelse (x==0,pseudocount,x))
+
+  for(i in 1:matrixColSize) {
+    for(j in 1:matrixColSize) {
+      resultsMatrix[i,j]=JSD(as.vector(inMatrix[,i]),
+                             as.vector(inMatrix[,j]))
+    }
+  }
+  colnames -> colnames(resultsMatrix) -> rownames(resultsMatrix)
+  as.dist(resultsMatrix)->resultsMatrix
+  attr(resultsMatrix, "method") <- "dist"
+  return(resultsMatrix)
+
+}
+
+
+
+#' kBest
+#' to get the best k in non-supervision method
+#' @param data
+#' @param dist
+#' @param method
+#'
+#' @return list , include the best cluster number k & original figure & dataframe
+#' @export
+#'
+#' @examples
+kBest <- function(data, dist , method = "kmeans"){
+
+  nclusters=NULL
+  sil = NULL
+  out <- list()
+  res <- matrix(NA, 19, ncol(data))
+
+  for (k in 2:20) {
+    #print(k)
+    switch (method,
+            kmeans = { data.cluster_temp <-kmeans(dist, k)$cluster},
+            pam = { data.cluster_temp <-pam(dist, k)$clustering},
+            fanny = {  data.cluster_temp <- fanny(dist, k)$clustering}
+    )
+    res[k-1,] <- data.cluster_temp
+    nclusters[k-1] <- index.G1(t(data) , data.cluster_temp,  d = dist,
+                               centrotypes = "medoids")
+    sil[k-1] <- mean(silhouette(data.cluster_temp, dist = dist)[,3])
+  }
+
+  best <- which.max(nclusters)+1
+  kCluster <- c(2:20)
+  CH_index <- nclusters
+  Silhouette <- sil
+  cluster <- data.frame(kCluster,  CH_index, Silhouette)
+  cluster <- melt(cluster, id = "kCluster")
+  colnames(cluster) <- c("kCluster", "Index", "value")
+
+  figure <- ggplot(cluster, aes(x=value, y=kCluster))+
+    geom_segment(aes(yend=kCluster),xend=0,colour="grey")+
+    geom_point(size=3,aes(colour=Index))+
+    scale_colour_brewer(palette="Set1",limits=c("CH_index","Silhouette"))+
+    theme_bw()+finalTheme+xlab("")+ylab("Number of cluster")+facet_grid(.~Index, scales = "free")
+
+  out <- list(res[best-1,], best, figure)
+
+  return(out)
+
+}
+
+
+#' kBest2
+#'
+#' @param data
+#' @param dist
+#' @param method
+#' @param clustNum
+#'
+#' @return
+#' @export
+#'
+#' @examples
+kBest2 <- function(data, dist , method = "kmeans",  clustNum){
+
+  nclusters=NULL
+  sil = NULL
+  out <- list()
+  res <- matrix(NA, clustNum, ncol(data))
+
+  for (k in 1:clustNum) {
+
+    switch (method,
+            kmeans = { data.cluster_temp <-kmeans(dist, k)$cluster},
+            pam = { data.cluster_temp <- pam(dist, k)$clustering},
+            fanny = {  data.cluster_temp <- fanny(dist, k)$clustering}
+    )
+
+    res[k,] <- data.cluster_temp
+    nclusters[k] <- index.G1(t(data) , data.cluster_temp,  d = dist,
+                             centrotypes = "medoids")
+
+  }
+
+  best <- which.max(nclusters)
+  print("Best cluster number is ", best)
+  CH_index <- nclusters
+  df <- data.frame(clusters = as.factor(1:clustNum), y = CH_index)
+  df[1,2] <- min(df[,2], na.rm=T)-0.1*min(df[,2], na.rm=T)
+  ylab <- "Calinski-Harabasz index"
+
+  p <- ggpubr::ggline(df, x = "clusters", y = "y", group = 1,
+                      color = "steelblue", ylab = ylab,
+                      xlab = "Number of clusters k",
+                      main = "Optimal number of clusters\nCH-index method"
+  )
+
+  p <- p + geom_vline(xintercept = which.max(CH_index), linetype=2, color = "steelblue")
+
+  return(p)
+
+}
+
+
+#' multiAdonis
+#' for pair compare for multigroup use the adonis
+#' @param dat
+#' @param config
+#' @param perm
+#' @param method
+#'
+#' @return data.frame
+#' @export
+#'
+#' @examples
+multiAdonis <- function(dat, config, perm=999, method="bray"){
+  # reorder the sample id of dat and config
+  # dat : row is sample
+  # perm : permutation number ,default 999
+  # method : distance method
+
+  id <- intersect(rownames(dat), rownames(config))
+  dat <- dat[id, ]
+  config <- config[id,]
+  combng <- combn(unique(config), 2)
+  res <- matrix(NA, nrow=ncol(combng), ncol = 6)
+  for(i in 1:ncol(combng)){
+    group <- c(config[which(config==combng[1,i])],
+               config[which(config==combng[2,i])])
+    pro <- dat[c(which(config==combng[1,i]),
+                 which(config==combng[2,i])), ]
+    # here use the adonise function
+    library(vegan)
+    res[i, 1:6] <- as.numeric(adonis(pro~group, permutations = perm, method = method)$aov[1,])
+  }
+  rownames(res) <- apply(combng, 2, function(x){paste0(x[1], " vs ", x[2])})
+  colnames(res) <- c("Df", "SumsOfSqs", "MeanSqs", "F.Model", "R2", "Pr(>F)")
+  return(res)
+
+}
+
